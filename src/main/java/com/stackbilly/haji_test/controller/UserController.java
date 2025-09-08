8c4271e1f38e3585790b82c7ff68a0012636a69e
Stackbilly <livingstoneochadia@gmail.com>
2025-09-08 13:45:27 +0300

    ""

--- src/main/java/com/stackbilly/haji_test/controller/UserController.java
+++ src/main/java/com/stackbilly/haji_test/controller/UserController.java
@@ -3,15 +3,15 @@ package com.stackbilly.haji_test.controller;
 import java.util.List;
 import java.util.UUID;
 
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.stackbilly.haji_test.models.User;
@@ -22,7 +22,7 @@ import com.stackbilly.haji_test.service.UserService;
 public class UserController {
 
     @Autowired
-    private UserService userService;
+    final private UserService userService;
 
     public UserController(UserService uService) {
         this.userService = uService;
